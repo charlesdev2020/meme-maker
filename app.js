65c283df2668525fbf031c5e9302b8a7a091eedd
charlesdev2020 <charlesdev2020@gmail.com>
2022-12-05 00:33:03 +0900

    #2. LineWidth & Paint Color

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option") // HTMLCollection 배열 생성
+);
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas"); // Javascript에서 Canvas element에 접근
 const ctx = canvas.getContext("2d"); // Brush. Canvas에 그림을 그릴 때 사용
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = 5;
 let isPainting = false;
 
 function onMove(event) {
@@ -22,10 +27,33 @@ function startPainting(event) {
 }
 
 function cancelPainting(event) {
+  ctx.beginPath();
   isPainting = false;
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  console.dir(event.target.dataset.color); // 색상 Hex 코드를 가져옴
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue; // color Input의 색상을 변경
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); // canvas 영역 밖으로 나갈 때
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
