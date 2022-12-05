3bf556dd0bb1c11cbf21e3cdac2706a478dc7532
charlesdev2020 <charlesdev2020@gmail.com>
2022-12-06 00:41:40 +0900

    #2. Filling Mode & Eraser

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+
+// Foreach 함수를 사용하기 위해 배열로 생성
 const colorOptions = Array.from(
-  document.getElementsByClassName("color-option") // HTMLCollection 배열 생성
+  document.getElementsByClassName("color-option") // HTMLCollection을 Return
 );
+
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas"); // Javascript에서 Canvas element에 접근
 const ctx = canvas.getContext("2d"); // Brush. Canvas에 그림을 그릴 때 사용
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
-ctx.lineWidth = 5;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -42,18 +52,51 @@ function onColorChange(event) {
 
 function onColorClick(event) {
   console.dir(event.target.dataset.color); // 색상 Hex 코드를 가져옴
-  const colorValue = event.target.dataset.color;
+  const colorValue = event.target.dataset.color; // Color : HTML의 data-*에서 *
   ctx.strokeStyle = colorValue;
   ctx.fillStyle = colorValue;
   color.value = colorValue; // color Input의 색상을 변경
+}
+
+function onModeClick(event) {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestoryClick() {
+  ctx.fillStyle = "white";
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  //ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); // canvas 영역 밖으로 나갈 때
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
+// Foreach를 사용해 onColorClick 함수를 가진 ClickEventListener를 추가
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestoryClick);
+eraserBtn.addEventListener("click", onEraserClick);
